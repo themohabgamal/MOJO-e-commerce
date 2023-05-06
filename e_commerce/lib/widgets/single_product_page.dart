46ab038156575983b7c99118f309f1316ed46eae
mohab gamal <themohabgamal@users.noreply.github.com>
2023-05-06 15:21:47 +0300

    single product page done

--- e_commerce/lib/widgets/single_product_page.dart
+++ e_commerce/lib/widgets/single_product_page.dart
@@ -1,7 +1,10 @@
 import 'package:e_commerce/business_logic/home/bloc/home_bloc.dart';
 import 'package:e_commerce/models/category_response_model.dart';
+import 'package:e_commerce/theming/theme.dart';
 import 'package:e_commerce/widgets/home_single_product_args.dart';
 import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
+import 'package:readmore/readmore.dart';
 
 class SingleProductPage extends StatelessWidget {
   static const String routeName = 'singleProdPage';
@@ -17,6 +20,13 @@ class SingleProductPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("MOJO"),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(IconlyLight.arrow_left_2, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
         actions: [
           IconButton(
             icon: Image.asset(
@@ -29,16 +39,112 @@ class SingleProductPage extends StatelessWidget {
           )
         ],
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 20),
-          Text(
-            "${args.categoryResponseModel?.title}",
-            style: Theme.of(context).textTheme.headline4?.copyWith(
-                  color: Colors.black,
-                ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Text(
+                "${args.categoryResponseModel?.title}",
+                style: Theme.of(context).textTheme.headline4?.copyWith(
+                    color: Colors.black, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "${args.categoryResponseModel?.category}",
+                style: Theme.of(context).textTheme.headline6?.copyWith(
+                    fontSize: 22,
+                    color: MyTheme.darkGreyColor,
+                    fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 10),
+              Image.network(
+                "${args.categoryResponseModel?.image}",
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.contain,
+              ),
+              SizedBox(height: 10),
+              Text(
+                "\$ ${args.categoryResponseModel?.price}",
+                style: Theme.of(context).textTheme.subtitle2?.copyWith(
+                    fontSize: 28,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(height: 10),
+              ReadMoreText(
+                "${args.categoryResponseModel?.description}",
+                style: Theme.of(context)
+                    .textTheme
+                    .subtitle1
+                    ?.copyWith(color: MyTheme.darkGreyColor),
+                trimLines: 2,
+                colorClickableText: MyTheme.mainColor,
+                lessStyle: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: MyTheme.mainColor),
+                trimMode: TrimMode.Line,
+                trimCollapsedText: ' Show more',
+                trimExpandedText: ' Show less',
+                moreStyle: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: MyTheme.mainColor),
+              ),
+            ],
           ),
-        ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: BottomAppBar(
+            color: Colors.transparent,
+            elevation: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 70,
+                  height: 70,
+                  decoration: BoxDecoration(
+                      color: Colors.grey.withAlpha(40),
+                      borderRadius: BorderRadius.circular(50)),
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        IconlyLight.heart,
+                        size: 30,
+                      )),
+                ),
+                Container(
+                    width: 200,
+                    height: 70,
+                    decoration: BoxDecoration(
+                        color: MyTheme.mainColor,
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              IconlyLight.bag,
+                              size: 30,
+                              color: Colors.white,
+                            )),
+                        Text("Add to cart",
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline5
+                                ?.copyWith(color: Colors.white))
+                      ],
+                    )),
+              ],
+            )),
       ),
     );
   }
